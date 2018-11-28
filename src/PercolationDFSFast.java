6a2744e873f79d1a084bbee9d5afdede6750198c
Shreyas Kulkarni <srk37@duke.edu>
2018-11-28 13:54:47 -0500

    wfwfwe

---
+++ src/PercolationDFSFast.java
@@ -0,0 +1,61 @@
+
+public class PercolationDFSFast extends PercolationDFS{
+
+	public PercolationDFSFast(int n) {
+		super(n);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void updateOnOpen(int row , int col) { 
+		
+		if(row == 0) { 
+			dfs(row , col); 
+		}
+		
+		else if (row == myGrid.length -1) { 
+			if(col == 0) {  
+				if(isFull(row - 1, col) || isFull(row , col +1)) { 
+					dfs(row,col); 
+					}
+				}
+			
+			else if(col == myGrid.length -1) {
+				if(isFull(row - 1, col) || isFull(row , col -1)) { 
+					dfs(row,col); 
+					}
+				}
+			else { 
+				if(isFull(row - 1 , col) || isFull(row , col -1) || isFull(row , col +1)) { 
+					dfs(row , col);
+					}
+				}
+			}
+		
+		else {  
+			if(col == 0) { 
+				if(isFull(row-1 , col) || isFull(row +1 , col) || isFull(row , col + 1)) { 
+						dfs(row, col);
+					}	
+				}
+			else if(col == myGrid.length -1) {
+				if(isFull(row -1 , col) || isFull(row +1 , col) || isFull(row , col - 1)) { 
+					dfs(row, col);
+					}
+				}
+			else { 
+				if(isFull(row - 1, col) || isFull(row , col - 1) || isFull(row + 1, col) || isFull(row , col +1)) { 
+					dfs(row, col); 
+				}
+
+			}
+			
+				
+			
+			
+			
+			}
+		
+	}
+	
+
+}
